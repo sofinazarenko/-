fd68fc3cf31aa1e2021d2161e9ba5c486caa2ac1
lucky13 <78902599+sofinazarenko@users.noreply.github.com>
2021-05-20 23:22:51 +0300

    Лабораторная работа 7

---
+++ lab7/URLDepthPair.java
@@ -0,0 +1,22 @@
+public class URLDepthPair {
+    private String url;
+    private int depth;
+    private int visited;
+    public URLDepthPair(String URL, int dep) {
+        url = URL;
+        depth=dep;
+        visited=1;
+    }
+    public String getURL() {
+        return url;
+    }
+    public int getDepth() {
+        return depth;
+    }
+    public void incrementVisited() {
+        visited++;
+    }
+    public String toString() {
+        return "<URL href=\"" + url + "\" visited=\"" + visited + "\" depth=\"" + depth + "\" \\>";
+    }
+}
